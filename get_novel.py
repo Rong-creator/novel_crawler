c4b63db63a39d72a70c54d6d7c933dc7612169e5
Rong-creator <u6364576@anu.edu.au>
2020-10-08 00:00:02 +1100

    grab novel content in website

--- get_novel.py
+++ get_novel.py
@@ -1,14 +1,28 @@
 import requests as req
+from bs4 import BeautifulSoup
 import __settings__ 
+import re
 
 
 def get_page(url):
 	resp = req.get(url)
 	return resp.text
 
+def grab_content(page,selector):
+	soup = BeautifulSoup(page, 'html.parser')
+	elements = soup.select(selector)
+	return elements
+
+def remove_all_tags(content):
+	pattern = re.compile(r'\s|<.*?>',re.S)
+	result = pattern.sub('', content)
+	return result
 
 def main():
-	print(get_page(__settings__.novel_website))
+	page = get_page("https://www.biquge.com.cn/book/31833/489624.html")
+	selector = "#content"
+	content = grab_content(page,selector)
+	print(remove_all_tags(str(content)))
     
 
 if __name__ == "__main__":
